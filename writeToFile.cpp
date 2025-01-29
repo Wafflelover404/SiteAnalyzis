5b3d9787dbfd67381d28b9b2a1afff86d94d5fcf
John Afanasieff <118971918+Wafflelover404@users.noreply.github.com>
2025-01-29 07:50:41 -0500

    Update writeToFile.cpp

--- writeToFile.cpp
+++ writeToFile.cpp
@@ -1,26 +1,60 @@
-#include <fstream>
+#include <iostream>
 #include <string>
+#include <vector>
+#include "writeToFile.cpp"
 
-void writeToFile(std::string content, std::string filepath) {
-    std::ofstream file(filepath, std::ios::out);
-    if (file.is_open()) {
-        file << content;
+std::vector <std::string> getTagStructure (std::string html) {
+    if (html.empty ()) return {};
+    std::vector <std::string> tagStructure;
+    for (int32_t it = 0; it < html.length(); ++it) {
+        if (html[it] == '<') {
+            std::string tag = "";
+            int32_t j = it;
+            do {
+                tag += html[j];
+                j ++;
+                if (html[j] == ' ') {tag+= '>'; break;}
+            } while (html[j - 1] != '>');
+            tagStructure.push_back(tag);
+        }
+    }
+    return tagStructure;
+} 
+
+void printTagStructure (std::vector <std::string> tagStructure) {
+    if (tagStructure.empty()) return;
+    int32_t tabs = 0;
+    for (std::string tag : tagStructure) {
+        std::string currentTagName = "";
+        if (tag[1] == '/') {
+            for (int it = 2; tag[it] != '>'; ++it) {
+                currentTagName += tag[it];
+            }
+        }
+        else {
+            for (int it = 1; tag[it] != '>'; ++it) {
+                currentTagName += tag[it];
+            }
+        }
+
+        if (tag.length() >= 2 && tag[1] == '/') {
+            tabs -= 2;
+        } else {
+            tabs += 2;
+        }
+
+        for (int i = 0; i < tabs; ++i) {
+            std::cout << ' ';
+        }
+        std::cout << tag << '\n';
     }
 }
 
-std::string readFromFile (std::string filepath) { // Fixed multiline input (actually most of them are) std::string
-    std::ifstream file(filepath);
-    if (file.is_open()) {
-        std::string content = "";
-        std::string line;
-        while (std::getline(file, line)) content += line + "\n";
-        return content;
-    }
-    return "Wrong filepath or file not found.";
+int32_t main () {
+    std::string htmlExample = readFromFile("./response.txt");
+    std::vector <std::string> tags = getTagStructure(htmlExample);
+    // for (std::string tag : tags) {
+    //     std::cout << tag << '\n';
+    // }
+    printTagStructure(tags);
 }
-
-
-// #include <iostream>
-// int32_t main () {
-//     std::cout << readFromFile("response.txt");
-// }
